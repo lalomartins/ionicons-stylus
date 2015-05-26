0f1294436b45764744832e3fa5ec82c2097dd0f9
Lalo Martins <lalo.martins@gmail.com>
2015-05-26 22:41:27 +0200

    stylus-multi instead of s-jeet

--- package.js
+++ package.js
@@ -8,13 +8,13 @@ Package.describe({
 Package.onUse(function(api) {
   //disabled versionsFrom for now - causing trouble on windows
   //api.versionsFrom('1.0');
-  
+
   //api.use("stylus", ["server"]);
   //api.imply("stylus", ["server"]);
   //using s-jeet since the stylus package is currently out of date
   //https://github.com/meteor/meteor/pull/3749
-  api.use("juliancwirko:s-jeet", ["server"]);
-  api.imply("juliancwirko:s-jeet", ["server"]);
+  api.use("cryptoquick:stylus-multi", ["server"]);
+  api.imply("cryptoquick:stylus-multi", ["server"]);
 
   api.addFiles([
     'fonts/ionicons.eot',
